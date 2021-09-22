6922ff72cf6532624edcd86f4e613b5bca66b97f
victor Farias <victorfarias.tv@gmail.com>
2021-09-22 08:40:56 -0300

    Updates

---
+++ precache-manifest.730f6d1bf97e34b42a677d374d1a14d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3363690c3d2e1cdfbe7ab63c4b081ea7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a859bc73d8fa5cada6d",
+    "url": "/static/css/main.da8d717f.chunk.css"
+  },
+  {
+    "revision": "285783175e19c569b8f2",
+    "url": "/static/js/2.f70bd6bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f70bd6bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a859bc73d8fa5cada6d",
+    "url": "/static/js/main.ab39f528.chunk.js"
+  },
+  {
+    "revision": "9c403c586794b3d35b05",
+    "url": "/static/js/runtime-main.e3cf0977.js"
+  }
+]);
