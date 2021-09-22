b7c8e6f1d70e6b3452bdbb491c7c6e50270bf7c1
victor Farias <victorfarias.tv@gmail.com>
2021-09-22 08:35:01 -0300

    Inicial Version

---
+++ src/components/MovieList.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <section className="movie-list">
+        {movies.map((card) => (
+          <MovieCard movie={ card } key={ card.title } />
+        ))}
+      </section>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default MovieList;
